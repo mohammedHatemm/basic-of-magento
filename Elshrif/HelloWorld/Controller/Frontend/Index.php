a241664e4169997ee71a1b6a29b3cb4b1d7c2554
mohamed elsherif <mohamed.hatem97825@gmail.com>
2025-12-23 10:06:52 +0200

    index and save controller

--- Elshrif/HelloWorld/Controller/Frontend/Index.php
+++ Elshrif/HelloWorld/Controller/Frontend/Index.php
@@ -1,1 +1,27 @@
 <?php
+declare(strict_types=1);
+
+namespace Elshri\HelloWorld\Controller\Frontend;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\View\Result\Page;
+
+class Index implements HttpGetActionInterface {
+
+    public function __construct(
+        PageFactory $pageFactory,
+        RequestInterface $request,
+    ){}
+
+    public function execute():page
+    {
+        $id = $this->request->getParam('id');
+        $page = $pageFactory->create();
+        $page->getConfig()->getTitle()->set(__('Hello World'));
+        return $page;
+
+    }
+
+}
--- Elshrif/HelloWorld/Controller/Frontend/Save.php
+++ Elshrif/HelloWorld/Controller/Frontend/Save.php
@@ -1,1 +1,43 @@
 <?php
+declare(strict_types=1);
+namespace Elshrif\HelloWorld\Controller\Frontend;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\Result\Redirect;
+use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\Data\Form\FormKey\Validator;
+
+
+class Save implements HttpPostActionInterface
+{
+    public function __construct(
+        private RequestInterface $request,
+        private RedirectFactory $redirectFactory,
+        private Validator $formKeyValidator,
+        private ManagerInterface $messageManager
+    ){}
+
+    public function execute():Redirect
+    {
+        $redirect = $this->redirectFactory->create();
+        if(!$this->formKeyValidator->validate($this->request)) {
+            $this->messageManager->addErrorMessage(__('Invalid request'));
+            return $redirect->setPath('*/*/');
+
+        }
+        try {
+            $data = $this->request->getPostValue();
+            //the data you want to save it
+
+            $this->messageManager->addSuccessMessage(__('Your information has been updated.'));
+            $redirect->setPath('*/*/index');
+            return $redirect;
+        }
+        catch (\Exception $e) {
+            $this->messageManager->addErrorMessage($e->getMessage());
+            $redirect->setPath('*/*/');
+        }
+    }
+}
