e2c6792bc356538ccdad4e40889d4b7b09ced54d
mohamed elsherif <mohamed.hatem97825@gmail.com>
2025-12-29 08:03:12 +0200

    admin controller

--- Elshrif/HelloWorld/Controller/Adminhtml/Entity/Index.php
+++ Elshrif/HelloWorld/Controller/Adminhtml/Entity/Index.php
@@ -1,1 +1,18 @@
 <?php
+namespace Elshrif\HelloWorld\Controller\Adminhtml\Entity;
+
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+class Index implements  HttpGetActionInterface
+{
+
+    public function __construct()
+    {
+
+    }
+    public function execute()
+    {
+
+    }
+}
