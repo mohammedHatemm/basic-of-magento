49dcf923b92740b3759f6a6f9e9281005c37cc0e
mohamed elsherif <mohamed.hatem97825@gmail.com>
2025-12-31 06:45:49 +0200

    model in magenot and resource model

--- Elshrif/HelloWorld/Model/Entity.php
+++ Elshrif/HelloWorld/Model/Entity.php
@@ -7,12 +7,40 @@ use Elshrif\HelloWorld\Api\Data\EntityInterface;
 use Elshrif\HelloWorld\Model\ResourceModel\Entity AS ResourceModel;
 
 
-class Entity extends AbstractModel implements EntityInterface , IdentityInterface
+class Entity extends AbstractModel implements EntityInterface, IdentityInterface
 {
+
+    public const CACHE_TAG = 'hello_world_entity';
+    protected $_chacheTag = self::CACHE_TAG;
+    protected  $_eventPrefix = 'hello_world_entity';
+    protected $_eventObject = 'entity';
+
 
     protected function _construct()
     {
+        $this->_init(ResourceModel::class);
 
+    }
+
+    public function getIdentities()
+    {
+        // TODO: Implement getIdentities() method.
+        return [self::CACHE_TAG . '_' . $this->getId()];
+    }
+
+    public function getEntityId():?int
+    {
+        return $this->getData(self::ENTITY_ID) ? (int) $this->getData(self::ENTITY_ID) : null;
+    }
+
+    public function setEntityId(int $entityId):EntityInterface
+    {
+        return $this->setData(self::ENTITY_ID , $entityId);
+    }
+
+    public function getName():?string
+    {
+        return $this->getData(self::NAME);
     }
 
 }
--- Elshrif/HelloWorld/Model/ResourceModel/Entity.php
+++ Elshrif/HelloWorld/Model/ResourceModel/Entity.php
@@ -8,5 +8,5 @@ use Magento\Framework\Model\ResourceModel\Db\VersionControl\AbstractDb;
 
 class Entity extends AbstractDb {
 
-    
+
 }
