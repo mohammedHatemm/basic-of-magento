b21d3c520866746e58ea2fa1d1b15a86101d9a20
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-01-03 17:03:34 +0200

    update PointRepository file

--- Elshrif/Loyalty/Model/PointsRepository.php
+++ Elshrif/Loyalty/Model/PointsRepository.php
@@ -1,114 +1,178 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Elshrif\Loyalty\Model;
-use Elshrif\Loyalty\Model\PointsFactory;
+
+use Elshrif\Loyalty\Api\PointsRepositoryInterface;
+use Elshrif\Loyalty\Api\Data\PointsInterface;
 use Elshrif\Loyalty\Model\ResourceModel\Points as PointsResource;
 use Elshrif\Loyalty\Model\ResourceModel\Points\CollectionFactory;
+use Magento\Framework\Exception\CouldNotDeleteException;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\LocalizedException;
 use Psr\Log\LoggerInterface;
 
-
-class PointsRepository
+class PointsRepository implements PointsRepositoryInterface
 {
     public function __construct(
         private PointsFactory $pointsFactory,
         private PointsResource $pointsResource,
         private CollectionFactory $collectionFactory,
         private LoggerInterface $logger
-    )
-    {}
+    ) {}
 
-    /*
-     * GEt points by Customer
-     * */
+    /**
+     * {@inheritdoc}
+     */
+    public function getById(int $entityId): PointsInterface
+    {
+        $points = $this->pointsFactory->create();
+        $this->pointsResource->load($points, $entityId);
 
-    public function getByCustomerId(int $customerId): Points
+        if (!$points->getEntityId()) {
+            throw new NoSuchEntityException(
+                __('Points record with ID "%1" does not exist.', $entityId)
+            );
+        }
+        return $points;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCustomerId(int $customerId): PointsInterface
     {
         $collection = $this->collectionFactory->create();
         $collection->addFieldToFilter('customer_id', $customerId);
-        /** @var Points $points */
 
+        /** @var PointsInterface $points */
         $points = $collection->getFirstItem();
-        if (!$points ->getEntityId()) {
-            $points= $this->pointsFactory->create();
-            $points->setCustomerId($customerId);
-            $points->setPointsBalance(0);
-            $points->setTotalSpent(0);
-            $points->setTotalEarned(0);
 
+        if (!$points->getEntityId()) {
+            throw new NoSuchEntityException(
+                __('Points record for customer "%1" does not exist.', $customerId)
+            );
         }
         return $points;
-
-
     }
 
-    /*
-     * save Points
-     * */
-
-
-    public function save(Points $points): Points
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrCreate(int $customerId): PointsInterface
     {
+        try {
+            return $this->getCustomerId($customerId);
+        } catch (NoSuchEntityException $e) {
+            $points = $this->pointsFactory->create();
+            $points->setCustomerId($customerId)
+                ->setPointsBalance(0)
+                ->setTotalEarned(0)
+                ->setTotalSpent(0);
+            return $points;
+        }
+    }
 
-        try{
+    /**
+     * {@inheritdoc}
+     */
+    public function save(PointsInterface $points): PointsInterface
+    {
+        try {
             $this->pointsResource->save($points);
-        }
-        catch (\Exception $exception){
-            $this->logger->error( 'Error saving points'.$exception->getMessage());
-            throw $exception;
+        } catch (\Exception $e) {
+            $this->logger->error('Error saving points: ' . $e->getMessage());
+            throw new CouldNotSaveException(
+                __('Could not save points: %1', $e->getMessage())
+            );
         }
         return $points;
-
     }
 
-    /*
-     * add points to Customer
-     * */
-
-    public function addPoint(int $customerId , int $pointsToAdd , string $reason=''): Points
+    /**
+     * {@inheritdoc}
+     */
+    public function delete(PointsInterface $points): PointsInterface
     {
-        $points = $this->getByCustomerId($customerId);
-        $newBalancePoint=$points->getPointsBalance() + $pointsToAdd;
-        $newTotalEarnedPoints = $points->getTotalEarnings() + $pointsToAdd;
-        $points->setPointsBalance($newBalancePoint);
-        $points->setTotalEarned($newTotalEarnedPoints);
-        $this->pointsResource->save($points);
+        try {
+            $this->pointsResource->delete($points);
+        } catch (\Exception $e) {
+            throw new CouldNotDeleteException(
+                __('Could not delete: %1', $e->getMessage())
+            );
+        }
+        return $points;
+    }
 
-        $this->logger->info(sprintf('Add %d Points to customer %d . Reason: %s',
-            $pointsToAdd,
+    /**
+     * {@inheritdoc}
+     */
+    public function deleteById(int $entityId): bool
+    {
+        $points = $this->getById($entityId);
+        $this->delete($points);
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addPoints(int $customerId, int $points, string $reason = ''): PointsInterface
+    {
+        $pointsRecord = $this->getOrCreate($customerId);
+
+        $newBalance = $pointsRecord->getPointsBalance() + $points;
+        $newTotalEarned = $pointsRecord->getTotalEarned() + $points;
+
+        $pointsRecord->setPointsBalance($newBalance)
+            ->setTotalEarned($newTotalEarned);
+
+        $this->save($pointsRecord);
+
+        $this->logger->info(sprintf(
+            'Added %d points to customer %d. Reason: %s. New balance: %d',
+            $points,
             $customerId,
-            $reason
-            )
-        );
+            $reason,
+            $newBalance
+        ));
 
-
-        return $points;
-
-
+        return $pointsRecord;
     }
 
-    /*
-     * spent points
-     * */
-    public function spendPoints(int $customerId , int $pointsToSpend , string $reason= ''): Points
+    /**
+     * {@inheritdoc}
+     */
+    public function spendPoints(int $customerId, int $pointsToSpend, string $reason = ''): PointsInterface
     {
-        $points = $this->getByCustomerId($customerId);
-        if($points->getPointsBalance() < $pointsToSpend){
-            throw new \Exception('Points balance is not enough to spend');
+        $pointsRecord = $this->getOrCreate($customerId);
+
+        if ($pointsRecord->getPointsBalance() < $pointsToSpend) {
+            throw new LocalizedException(__(
+                'Insufficient points. Balance: %1, Required: %2',
+                $pointsRecord->getPointsBalance(),
+                $pointsToSpend
+            ));
         }
 
-        $newBalance = $points->getPointsBalance() - $pointsToSpend;
-        $newTotalSpend = $points->getTotalSpent() + $pointsToSpend;
+        $newBalance = $pointsRecord->getPointsBalance() - $pointsToSpend;
+        $newTotalSpent = $pointsRecord->getTotalSpent() + $pointsToSpend;
 
-        $points->setPointsBalance($newBalance);
-        $points->setTotalSpent($newTotalSpend);
-        $this->pointsResource->save($points);
-        $this->logger->info(sprintf('Add %d Points to customer %d . Reason: %s',
-        $pointsToSpend,
-        $customerId,
-        $reason
+        $pointsRecord->setPointsBalance($newBalance)
+            ->setTotalSpent($newTotalSpent);
+
+        $this->save($pointsRecord);
+
+        $this->logger->info(sprintf(
+            'Spent %d points from customer %d. Reason: %s. New balance: %d',
+            $pointsToSpend,
+            $customerId,
+            $reason,
+            $newBalance
         ));
-        return $points;
 
+        return $pointsRecord;
     }
-
 }
