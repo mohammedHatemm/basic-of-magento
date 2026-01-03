d6111a41f1d4d3f8ab0665e8ea175a1ca27cf004
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-01-03 16:28:21 +0200

    update the resourceModel file

--- Elshrif/Loyalty/Model/ResourceModel/Points.php
+++ Elshrif/Loyalty/Model/ResourceModel/Points.php
@@ -1,10 +1,56 @@
 <?php
+
 namespace Elshrif\Loyalty\Model\ResourceModel;
+
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+
 class Points extends AbstractDb
 {
+
+
+    /**
+     *
+     *
+     *  @param string $tableName
+     * @param string $idFieldName
+     */
     protected function _construct()
     {
         $this->_init("elshrif_loyalty_points", "entity_id");
+    }
+
+
+    /**
+     * @param Points $model
+     * @param int $customerId
+     * @return $this
+     */
+
+    public function loadByCustomerId(
+        \Elshrif\Loyalty\Model\Points $model,
+        int $customerId
+    ): self {
+        $connection = $this->getConnection();
+        $select = $connection->select()
+            ->from($this->getMainTable())
+            ->where('customer_id = ?', $customerId);
+        $data = $connection->fetchRow($select);
+        if ($data) {
+            $model->setData($data);
+        }
+        return $this;
+    }
+
+    /**
+     * @param int $customerId
+     * @return bool
+     */
+    public function customerHasPoints(int $customerId): bool
+    {
+        $connection = $this->getConnection();
+        $select = $connection->select()
+            ->from($this->getMainTable(), ['entity_id'])
+            ->where('customer_id = ? ', $customerId);
+        return (bool) $connection->fetchOne($select);
     }
 }
