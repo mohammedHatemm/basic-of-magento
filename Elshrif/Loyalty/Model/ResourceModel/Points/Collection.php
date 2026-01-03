f5ab08bfe718e90c1b8c434bb841ed96ec3bb09b
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-01-03 16:47:09 +0200

    update the collection fill with filter

--- Elshrif/Loyalty/Model/ResourceModel/Points/Collection.php
+++ Elshrif/Loyalty/Model/ResourceModel/Points/Collection.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace Elshrif\Loyalty\Model\ResourceModel\Points;
-use Magento\Catalog\Model\ResourceModel\Collection\AbstractCollection;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use Elshrif\Loyalty\Model\Points as PointsModel;
 
 use Elshrif\Loyalty\Model\ResourceModel\Points as PointsResource;
@@ -9,8 +11,53 @@ use Elshrif\Loyalty\Model\ResourceModel\Points as PointsResource;
 
 class Collection extends AbstractCollection
 {
+
+    /**
+     * for indexing
+     */
+    protected $_idFieldName = 'entity_id';
+
+
+    protected $_eventPrefix = 'elshrif_loyalty_Points_collection';
+
+    protected $_eventObject = 'points_collection';
+
+    /**
+     * @param string $modelClass
+     * @param string $resourceClass
+     *
+     */
+
     protected function _construct()
     {
         $this->_init(PointsModel::class, PointsResource::class);
+    }
+
+    /**
+     *
+     * @param int|array $customerIds
+     * @return $this
+     */
+    public function addCustomerFillter($customerIds): self
+    {
+        if (is_array($customerIds)) {
+            $this->addFieldToFilter('customer_id', ['in' => $customerIds]);
+        } else {
+            $this->addFieldToFilter('cutomer_id', $customerIds);
+        }
+        return $this;
+    }
+
+    /**
+     * @return int
+     *
+     */
+
+    public function getToalPointsBalance(): int
+    {
+        $this->getSelect()
+            ->reset(\Zend_Db_Select::COLUMNS)
+            ->columns(['total' => new \Zend_Db_Expr('SUM(Points_balance)')]);
+        return (int) $this->getConnection()->fetchOne($this->getSelect());
     }
 }
